530c208ad1a987553ea126d755f384b41560e21f
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2015-06-01 13:43:03 +0200

    env.sh: cmdr now takes the arguments for Lin as a first argument

--- env.sh
+++ env.sh
@@ -1,9 +1,11 @@
 cmdr() {
+  local args="$1"
+  shift
   local dir="$1"
   shift
   for i; do
     i="${i%.ll}"
-    cmdrecord tests/"$dir/$i".t --no-stdin --source "$i".ll --env empty -- Lin --check "$i".ll
+    cmdrecord tests/"$dir/$i".t --no-stdin --source "$i".ll --env empty -- Lin $args "$i".ll
     mv "$i".ll fixtures/"$dir"
     git add fixtures/"$dir/$i".ll tests/"$dir/$i".t
   done
