9c586dc479aa7d220ee3a06370384e87962a2e3c
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2015-07-17 16:39:22 +0200

    Update Ling.Print.Instances

--- Ling/Print/Instances.hs
+++ Ling/Print/Instances.hs
@@ -11,7 +11,7 @@ instance Print a => Print (Arg a) where
     concatD [ doc (showString "(") , prt 0 ident , doc (showString ":")
             , prt 0 x , doc (showString ")") ]
 
-  prtList = concatD . map (prt 0)
+  prtList _ = concatD . map (prt 0)
 
 instance (Print a,Print b,Print c) => Print (a,b,c) where
   prt _i (a,b,c) =
@@ -21,28 +21,28 @@ instance (Print a,Print b,Print c) => Print (a,b,c) where
             , doc (showString ")") ]
 
 instance Print Session where
-  prt i   = prt i . reifySession
-  prtList = prtList . reifySessions
+  prt     i = prt i . reifySession
+  prtList i = prtList i . reifySessions
 
 instance Print RSession where
-  prt i   = prt i . reifyRSession
-  prtList = prtList . reifyRSessions
+  prt     i = prt i . reifyRSession
+  prtList i = prtList i . reifyRSessions
 
 instance Print Pref where
-  prt i = prt i . reifyPref
-  prtList = prtList . map reifyPref
+  prt     i = prt i . reifyPref
+  prtList i = prtList i . map reifyPref
 
 instance Print Proc where
-  prt i = prt i . reifyProc
-  prtList = prtList . map reifyProc
+  prt     i = prt i . reifyProc
+  prtList i = prtList i . map reifyProc
 
 instance Print Program where
-  prt i = prt i . reifyProgram
-  prtList = prtList . map reifyProgram
+  prt     i = prt i . reifyProgram
+  prtList i = prtList i . map reifyProgram
 
 instance Print Term where
-  prt i = prt i . reifyTerm
-  prtList = prtList . map reifyTerm
+  prt     i = prt i . reifyTerm
+  prtList i = prtList i . map reifyTerm
 {-  e = case e of
    Lit n -> prPrec i 3 (concatD [prt 0 n])
    Ann term0 term -> prPrec i 3 (concatD [doc (showString "(") , prt 0 term0 , doc (showString ":") , prt 0 term , doc (showString ")")])
@@ -53,7 +53,7 @@ instance Print Term where
    Proc ids proc -> prPrec i 0 (concatD [doc (showString "(") , prt 0 ids , doc (showString ")") , doc (showString ".") , prt 0 proc])
    Fun arg term -> prPrec i 0 (concatD [prt 0 arg , doc (showString " ->") , prt 0 term])
    Sig arg term -> prPrec i 0 (concatD [prt 0 arg , doc (showString " *") , prt 0 term])
-  prtList es = case es of
+  prtList _ es = case es of
    [] -> (concatD [])
    x:xs -> (concatD [prt 3 x , prt 0 xs])
 -}
