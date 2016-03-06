e5ad7f4e7fd0088be0ca0f335692ca6b6a64ead5
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-03-06 23:49:59 +0100

    regen-bnfc: now remove trailing whitespaces

--- bnfc-regen.sh
+++ bnfc-regen.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
-#echo MiniC; bnfc -d MiniC.cf
-#echo Ling;     bnfc -d Ling.cf
-# Past version for migration tool
-#echo Ling.Fmt.Albert; bnfc -p Ling.Fmt -d Ling/Fmt/Albert.cf
-#echo Ling.Fmt.Benjamin; bnfc -p Ling.Fmt -d Ling/Fmt/Benjamin.cf
+echo MiniC; bnfc -d MiniC.cf
+echo Ling; bnfc -d Ling.cf
+echo Ling.Fmt.Albert; bnfc -p Ling.Fmt -d Ling/Fmt/Albert.cf
+echo Ling.Fmt.Benjamin; bnfc -p Ling.Fmt -d Ling/Fmt/Benjamin.cf
 git ls-files -z '*.hs' '*.y' | xargs -0 -n 1 -- \
   sed -i -e 's/ \+$//' \
+         -e '$ { /^$/ d}' \
+         -e 's/^\Z//' \
          -e 's/^layoutSep   = ";"$/layoutSep   = ","/' \
          -e 's/^import\(  *\)Ling\..*\.ErrM$/import\1Ling.ErrM/'
 rm -f MiniC/ErrM.hs Ling/Fmt/*/ErrM.hs
-cabal build
